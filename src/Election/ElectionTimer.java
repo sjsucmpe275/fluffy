bb608f5e9e5bdcb1a106a2966590595317771450
codepenman <harish.kv12@gmail.com>
2016-03-31 16:05:37 -0700

    Added Election Timer file

--- src/Election/ElectionTimer.java
+++ src/Election/ElectionTimer.java
@@ -1,20 +1,54 @@
 package Election;
 
-import java.util.TimerTask;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class ElectionTimer extends TimerTask{
-	private Follower follower;
-	public ElectionTimer(Follower follower) {
-		this.follower=follower;
+import java.util.Date;
+
+/*
+* Gets a request to start election timer.
+* Know about the owner(person who starts it) of timer.
+* Owner should be able to cancel the task.
+* Owner should be notified once the timer is out
+* If owner cancel the task, timer should not notify the owner or should be interrupted
+* */
+public class ElectionTimer{
+
+	private static final Logger logger = LoggerFactory.getLogger ("Election Timer");
+	private final long electionTimeout;
+	private INodeState nodeState;
+	private TimerThread timerThread;
+
+	public ElectionTimer(INodeState nodeState, long electionTimeout) {
+		this.nodeState = nodeState;
+		this.electionTimeout = electionTimeout;
+		timerThread = new TimerThread ();
 	}
-	
-	@Override
-	public void run() {
-		long currentTime = System.currentTimeMillis ();
-		long diff=currentTime - follower.getLastHeartbeat();
-		
-		if(diff>follower.getDelay()){
-			follower.isElectionNeeded(true);
+
+	public void startTimer()   {
+		logger.info ("********Request to start election timer: " + new Date (System.currentTimeMillis ()));
+		timerThread.start ();
+	}
+
+	public void cancel()    {
+		logger.info ("********Request to cancel election timer: " + new Date (System.currentTimeMillis ()));
+		timerThread.interrupt ();
+	}
+
+	private class TimerThread extends Thread{
+
+		@Override
+		public void run(){
+			synchronized (this){
+				try {
+					logger.info ("********Election timer started: " + new Date (System.currentTimeMillis ()));
+					wait (electionTimeout);
+					logger.info ("********Election timed out: " + new Date (System.currentTimeMillis ()));
+					nodeState.onElectionTimeout ();
+				} catch (InterruptedException e) {
+					logger.info ("********Election Timer was interrupted: " + new Date (System.currentTimeMillis ()));
+				}
+			}
 		}
-	}	
+	}
 }
